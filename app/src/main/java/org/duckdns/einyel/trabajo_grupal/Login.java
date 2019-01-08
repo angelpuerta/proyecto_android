d6c4887ad1b4c587e2f46baee9130688be098a43
Kibian <32906528+Kibian@users.noreply.github.com>
2019-01-08 11:49:48 +0100

    Break para agilizar el proceso de inicio de sesion

--- app/src/main/java/org/duckdns/einyel/trabajo_grupal/Login.java
+++ app/src/main/java/org/duckdns/einyel/trabajo_grupal/Login.java
@@ -305,6 +305,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                         usuario = new User(Long.parseLong(id),
                                 userSnapshot.child("nick").getValue().toString(),
                                 userSnapshot.child("password").getValue().toString());
+                        break;
                     }
                 }
 
