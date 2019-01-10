2626da36906ed034a6c4cbcba779c825fb3a5cb6
Angel <angelpuertadiaz@gmail.com>
2019-01-10 15:07:03 +0100

    Antes de revert

--- app/src/main/java/org/duckdns/einyel/trabajo_grupal/Login.java
+++ app/src/main/java/org/duckdns/einyel/trabajo_grupal/Login.java
@@ -47,7 +47,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import org.duckdns.einyel.trabajo_grupal.model.User;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -55,9 +54,7 @@ import org.json.JSONObject;
 import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
 import android.util.Base64;
-
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.squareup.picasso.Picasso;
@@ -81,7 +78,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 
-public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener{
 
     protected EditText pw;
     protected EditText user;
@@ -115,10 +112,10 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
         //la inicializacion de twitter (tiene que ir aqui si o si)
         TwitterConfig config = new TwitterConfig.Builder(this)
-                .logger(new DefaultLogger(Log.DEBUG))
-                .twitterAuthConfig(new TwitterAuthConfig(getResources().getString(R.string.twitter_consumer_key), getResources().getString(R.string.twitter_consumer_secret)))
-                .debug(true)
-                .build();
+            .logger(new DefaultLogger(Log.DEBUG))
+            .twitterAuthConfig(new TwitterAuthConfig(getResources().getString(R.string.twitter_consumer_key), getResources().getString(R.string.twitter_consumer_secret)))
+            .debug(true)
+            .build();
 
         Twitter.initialize(config);
 
@@ -172,7 +169,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
 
                 final retrofit2.Call<com.twitter.sdk.android.core.models.User> userTw =
                         TwitterCore.getInstance().getApiClient(session).getAccountService()
-                                .verifyCredentials(true, false, false);
+                        .verifyCredentials(true, false, false);
 
                 userTw.enqueue(new Callback<com.twitter.sdk.android.core.models.User>() {
                     @Override
@@ -203,12 +200,12 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         });
     }
 
-    protected void facebookLoginProccess() {
+    protected void facebookLoginProccess(){
         callbackManager = CallbackManager.Factory.create();
 
         LoginButton loginButton = (LoginButton) findViewById(R.id.fbLogin);
 
-        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.setReadPermissions(Arrays.asList("public_profile" , "email"));
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -218,7 +215,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 GraphRequest graphRequest = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
-                        try {
+                       try {
                             user.setText("");
                             pw.setText("");
 
@@ -259,7 +256,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         signInButtonGoogle = findViewById(R.id.googleLogin);
         signInButtonGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 Intent intent = mGoogleSignInClient.getSignInIntent();
                 startActivityForResult(intent, GOOGLE_SIGN_IN_CODE);
             }
@@ -271,7 +268,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         //fb y twitter
         super.onActivityResult(requestCode, resultCode, data);
         //google
-        if (requestCode == GOOGLE_SIGN_IN_CODE) {
+        if(requestCode==GOOGLE_SIGN_IN_CODE){
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
 
             try {
@@ -281,7 +278,7 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                 user.setText("");
                 pw.setText("");
                 Intent mIntent = new Intent(getApplicationContext(), ListActivity.class);
-                if (account.getPhotoUrl() != null)
+                if(account.getPhotoUrl()!=null)
                     mIntent.putExtra("imageUrl", account.getPhotoUrl().toString());
                 mIntent.putExtra("username", account.getDisplayName());
                 mIntent.putExtra("socialLogin", "google");
@@ -302,13 +299,13 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         signIn(Suser, Spassword);
     }
 
-    protected void signIn(final String username, final String password) {
+    protected void signIn(final String username, final String password){
         users.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 User usuario = null;
-                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                    if (userSnapshot.child("nick").getValue().toString().equals(username) && userSnapshot.hasChild("id")) {
+                for(DataSnapshot userSnapshot: dataSnapshot.getChildren()) {
+                    if(userSnapshot.child("nick").getValue().toString().equals(username)) {
                         String id = userSnapshot.child("id").getValue().toString();
                         usuario = new User(Long.parseLong(id),
                                 userSnapshot.child("nick").getValue().toString(),
@@ -317,24 +314,27 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
                     }
                 }
 
-                if (!username.isEmpty()) {
-                    if (usuario != null) {
-                        if (usuario.getPassword().equals(password)) {
+                if(!username.isEmpty()){
+                    if(usuario != null){
+                        if(usuario.getPassword().equals(password)){
                             Intent mIntent = new Intent(getApplicationContext(), ListActivity.class);
                             mIntent.putExtra("socialLogin", "android");
                             mIntent.putExtra("username", user.getText().toString());
                             startActivity(mIntent);
                             user.setText("");
                             pw.setText("");
-                        } else {
+                        }
+                        else {
                             Toast.makeText(Login.this, "Credenciales incorrectas",
                                     Toast.LENGTH_SHORT).show();
                         }
-                    } else {
+                    }
+                    else {
                         Toast.makeText(Login.this, "Credenciales incorrectas",
                                 Toast.LENGTH_SHORT).show();
                     }
-                } else {
+                }
+                else {
                     Toast.makeText(Login.this, "Debe introducir un nombre de usuario",
                             Toast.LENGTH_SHORT).show();
                 }
@@ -348,18 +348,18 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
     }
 
     @Override
-    protected void onStop() {
+    protected void onStop(){
         super.onStop();
         LoginManager.getInstance().logOut();
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy(){
         super.onDestroy();
         LoginManager.getInstance().logOut();
     }
 
-    public void goSignUp(View view) {
+    public void goSignUp(View view){
         Intent mIntent = new Intent(getApplicationContext(), SignUp.class);
         startActivity(mIntent);
     }
@@ -368,14 +368,14 @@ public class Login extends AppCompatActivity implements GoogleApiClient.OnConnec
         try {
             PackageInfo info = getPackageManager().getPackageInfo("org.duckdns.einyel.trabajo_grupal",
                     PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
+            for(Signature signature: info.signatures){
                 MessageDigest md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
                 Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
             }
-        } catch (PackageManager.NameNotFoundException e) {
+        } catch (PackageManager.NameNotFoundException e){
             e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e){
             e.printStackTrace();
         }
     }
