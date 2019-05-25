7efe9cbc87e217d757cb426b9b1dc1bdc0b49008
AlegnaLav <alegnalav@gmail.com>
2019-05-25 21:42:28 +0200

    cambiado diseño pantalla perfil

--- app/src/main/java/org/duckdns/einyel/trabajo_grupal/Perfil.java
+++ app/src/main/java/org/duckdns/einyel/trabajo_grupal/Perfil.java
@@ -1,9 +1,11 @@
 package org.duckdns.einyel.trabajo_grupal;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,11 +23,7 @@ import com.squareup.picasso.Picasso;
 import org.duckdns.einyel.trabajo_grupal.model.User;
 import org.duckdns.einyel.trabajo_grupal.service.DownloadImageTask;
 import org.duckdns.einyel.trabajo_grupal.service.UserHolder;
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class Perfil extends AppCompatActivity {
 
@@ -38,24 +36,35 @@ public class Perfil extends AppCompatActivity {
     public static Long ID = 0L;
 
     private ImageView imgProfile;
-    private TextView name;
     private Button logOut;
     private Button edit;
     private Button change;
+    private TextView tvUserName;
+    private TextView tvBirthdateProfile;
+    private TextView tvGenderProfile;
 
     FirebaseDatabase database;
     DatabaseReference users;
     DatabaseReference usersSocial;
+
+    private final static String BASE_PROFILE_IMG = "https://firebasestorage.googleapis.com/v0/b/proyecto-moviles-86dc4.appspot.com/o/profile_icons%";
+    private final static String PROFILE_IMG_1 = BASE_PROFILE_IMG + "2F1.png?alt=media&token=d375045b-a13e-47c2-b1dc-5004d9903f5b";
+    private final static String PROFILE_IMG_2 = BASE_PROFILE_IMG + "2F2.png?alt=media&token=d2cbd9b2-6046-4776-885e-002f0cdd1e6b";
+    private final static String PROFILE_IMG_3 = BASE_PROFILE_IMG + "2F3.png?alt=media&token=a848c3dd-e6c6-4c82-acbb-810ee3cddaec" ;
+    private final static String PROFILE_IMG_4 = BASE_PROFILE_IMG + "2F4.png?alt=media&token=ed556508-9459-4618-95d8-703148108a74";
+    private final static String PROFILE_IMG_5 = BASE_PROFILE_IMG + "2F5.png?alt=media&token=6291b277-70b6-4c7f-b943-648950bfa480";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
         imgProfile = (ImageView) findViewById(R.id.profileImage);
-        name = (TextView) findViewById(R.id.name);
-        logOut = (Button) findViewById(R.id.LogOut);
+        tvUserName = (TextView) findViewById(R.id.tvUserName);
+        tvGenderProfile = (TextView) findViewById(R.id.tvGenderProfile);
+        tvBirthdateProfile = (TextView) findViewById(R.id.tvBirthdateProfile);
         edit = (Button) findViewById(R.id.buttoneditprofile);
         change = (Button) findViewById(R.id.buttonChangePw);
+        change.setPaintFlags(change.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         database = FirebaseDatabase.getInstance();
 
@@ -63,7 +72,7 @@ public class Perfil extends AppCompatActivity {
         usersSocial = database.getReference("usuariossociales");
 
         Bundle extras = getIntent().getExtras();
-        if(extras !=null) {
+        if (extras != null) {
             System.out.println("Extras: " + extras.getString("socialLogin") + " " + extras.getString("username"));
             LOGIN = UserHolder.getSocialLogin();
             NOMBRE_USUARIO = UserHolder.getUser().getNick();
@@ -73,12 +82,11 @@ public class Perfil extends AppCompatActivity {
             TWITTERID = UserHolder.getUser().getTwUsername();
             FACEBOOKID = UserHolder.getUser().getFbId();
         }
-        if(LOGIN.equals("android")){
+        if (LOGIN.equals("android")) {
             users.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     User usuario = null;
-                    String urlPic;
                     for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
                         if (userSnapshot.child("id").getValue().equals(ID) && userSnapshot.hasChild("id")) {
                             usuario = new User(ID,
@@ -90,11 +98,9 @@ public class Perfil extends AppCompatActivity {
                             break;
                         }
                     }
-                    new DownloadImageTask((ImageView) findViewById(R.id.socialImage))
-                            .execute("https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/android-512.png");
-
-                    name.setText("Nombre: " + usuario.getNick() + "\nSexo: " + usuario.getSexo() + "\nFecha de nacimiento: " + usuario.getNacimiento());
-
+                    tvUserName.setText(usuario.getNick());
+                    tvGenderProfile.setText(usuario.getSexo());
+                    tvBirthdateProfile.setText(usuario.getNacimiento());
                 }
 
                 @Override
@@ -102,8 +108,7 @@ public class Perfil extends AppCompatActivity {
                     //not implemented
                 }
             });
-        }
-        else {
+        } else {
             usersSocial.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -124,17 +129,12 @@ public class Perfil extends AppCompatActivity {
                         }
                     }
                     Log.d("Exception aaaaa:", usuario.getPfpUrl());
-                    if(LOGIN.equals("twitter")){
-                        new DownloadImageTask((ImageView) findViewById(R.id.socialImage))
-                                .execute("https://vignette.wikia.nocookie.net/es.starwars/images/9/92/Twitter_Icon.png");
-                    }
-                    else if(UserHolder.getSocialLogin().equals("facebook")){
-                        new DownloadImageTask((ImageView) findViewById(R.id.socialImage))
-                                .execute("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/600px-Facebook_logo_%28square%29.png");
-                    }
+
                     new DownloadImageTask((ImageView) findViewById(R.id.profileImage))
                             .execute(usuario.getPfpUrl());
-                    name.setText("Nombre: " + usuario.getNick() + "\nSexo: " + usuario.getSexo() + "\nFecha de nacimiento: " + usuario.getNacimiento());
+                    tvUserName.setText(usuario.getNick());
+                    tvGenderProfile.setText(usuario.getSexo());
+                    tvBirthdateProfile.setText(usuario.getNacimiento());
 
                 }
 
@@ -144,25 +144,6 @@ public class Perfil extends AppCompatActivity {
                 }
             });
         }
-        /**if(URL_PIC!=null) {
-         if (LOGIN.equals("facebook") || LOGIN.equals("twitter") || LOGIN.equals("google")) {
-         new DownloadImageTask((ImageView) findViewById(R.id.profileImage))
-         .execute(URL_PIC);
-         }
-         }
-
-         if(LOGIN.equals("twitter")){
-         new DownloadImageTask((ImageView) findViewById(R.id.socialImage))
-         .execute("https://vignette.wikia.nocookie.net/es.starwars/images/9/92/Twitter_Icon.png");
-         }
-         else if(LOGIN.equals("facebook")){
-         new DownloadImageTask((ImageView) findViewById(R.id.socialImage))
-         .execute("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Facebook_logo_%28square%29.png/600px-Facebook_logo_%28square%29.png");
-         }
-         else if(LOGIN.equals("google")){
-         new DownloadImageTask((ImageView) findViewById(R.id.socialImage))
-         .execute("https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-512.png");
-         }**/
 
 
         edit.setOnClickListener(new View.OnClickListener() {
@@ -172,10 +153,9 @@ public class Perfil extends AppCompatActivity {
                 mintent.putExtra("socialLogin", LOGIN);
                 mintent.putExtra("username", NOMBRE_USUARIO);
                 mintent.putExtra("id", ID);
-                if(LOGIN.equals("twitter")){
+                if (LOGIN.equals("twitter")) {
                     mintent.putExtra("twitterId", TWITTERID);
-                }
-                else if (LOGIN.equals("facebook")){
+                } else if (LOGIN.equals("facebook")) {
                     mintent.putExtra("facebookId", FACEBOOKID);
                 }
                 startActivityForResult(mintent, 111);
@@ -185,20 +165,18 @@ public class Perfil extends AppCompatActivity {
         change.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(LOGIN.equals("android")) {
+                if (LOGIN.equals("android")) {
                     Intent mintent = new Intent(getApplicationContext(), ChangePw.class);
                     mintent.putExtra("socialLogin", LOGIN);
                     mintent.putExtra("username", NOMBRE_USUARIO);
                     mintent.putExtra("id", ID);
-                    if(LOGIN.equals("twitter")){
+                    if (LOGIN.equals("twitter")) {
                         mintent.putExtra("twitterId", TWITTERID);
-                    }
-                    else if (UserHolder.getSocialLogin().equals("facebook")){
+                    } else if (UserHolder.getSocialLogin().equals("facebook")) {
                         mintent.putExtra("facebookId", FACEBOOKID);
                     }
                     startActivityForResult(mintent, 111);
-                }
-                else {
+                } else {
                     Toast.makeText(Perfil.this, "Esta función no está disponible para " +
                                     "inicios de sesión desde otras aplicaciones",
                             Toast.LENGTH_SHORT).show();
@@ -207,15 +185,40 @@ public class Perfil extends AppCompatActivity {
         });
 
 
-
-        logOut.setOnClickListener(new View.OnClickListener() {
+        /*logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finishAffinity();
                 Intent mIntent = new Intent(getApplicationContext(), Login.class);
                 startActivity(mIntent);
             }
-        });
+        });*/
+
+        setProfileImage();
+    }
+
+    private void setProfileImage(){
+        if (URL_PIC != null && !URL_PIC.equals("")) {
+            if (LOGIN.equals("facebook") || LOGIN.equals("twitter")) {
+                new DownloadImageTask((ImageView) findViewById(R.id.profileImage))
+                        .execute(URL_PIC);
+            }
+        }
+        else{
+            double random = Math.random();
+            Picasso.get().setLoggingEnabled(true);
+            if(random >= 0 && random <= 0.20){
+                Picasso.get().load(PROFILE_IMG_1).into(imgProfile);
+            }else if(random > 0.20 && random <= 0.40){
+                Picasso.get().load(PROFILE_IMG_2).into(imgProfile);
+            }else if(random > 0.40 && random <= 0.60){
+                Picasso.get().load(PROFILE_IMG_3).into(imgProfile);
+            }else if(random > 0.60 && random <= 0.80){
+                Picasso.get().load(PROFILE_IMG_4).into(imgProfile);
+            }else {
+                Picasso.get().load(PROFILE_IMG_5).into(imgProfile);
+            }
+        }
     }
 
     @Override
