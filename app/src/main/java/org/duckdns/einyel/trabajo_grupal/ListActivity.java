21aae08337cd7eb91ea7fadcfeaf3837c80fdc6b
AlegnaLav <alegnalav@gmail.com>
2019-05-25 16:28:34 +0200

    implementada opción de buscar eventos

--- app/src/main/java/org/duckdns/einyel/trabajo_grupal/ListActivity.java
+++ app/src/main/java/org/duckdns/einyel/trabajo_grupal/ListActivity.java
@@ -1,22 +1,25 @@
 package org.duckdns.einyel.trabajo_grupal;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
-import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
-import com.squareup.picasso.Picasso;
 
 import org.duckdns.einyel.trabajo_grupal.adapter.EventoAdapter;
 import org.duckdns.einyel.trabajo_grupal.listener.RecyclerTouchListener;
@@ -41,7 +44,6 @@ public class ListActivity extends AppCompatActivity implements EventoAdapter.Ass
     private RecyclerView recyclerView;
     private FirebaseRecyclerAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
-    private ImageButton profileButton;
     private int isOnCreate = 0;
     private User usuario;
 
@@ -84,6 +86,10 @@ public class ListActivity extends AppCompatActivity implements EventoAdapter.Ass
 
         setSpinnerCategoryFilterAdapter();
 
+    }
+
+    private void startSearchView(){
+        SearchView searchView = findViewById(R.id.app_bar_search);
     }
 
     private void setSpinnerCategoryFilterAdapter() {
@@ -134,34 +140,70 @@ public class ListActivity extends AppCompatActivity implements EventoAdapter.Ass
             }
         }
 
+        if(BUSCAR != null){
+            adapter = new EventoAdapter(App.get().filtrarEventos(BUSCAR),ListActivity.this);
+        }
+
+        filtrarEventos();
+
+    }
+
+    private void filtrarEventos(){
+
         recyclerView.setAdapter(adapter);
 
         recyclerView.addOnItemTouchListener(
-            new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
-                @Override
-                public void onClick(View view, int position) {
-                    Event evento = (Event) adapter.getItem(position);
-                    Intent nextActivity = new Intent(getApplicationContext(), DescripcionActivity.class);
-                    nextActivity.putExtra(EVENTO, evento);
-                    //USERNAME = extras.getString("username");
-                    nextActivity.putExtra("imageUrl", URL_PIC);
-                    nextActivity.putExtra("socialLogin", LOGIN);
-                    nextActivity.putExtra("username", USERNAME);
-                    startActivity(nextActivity);
-                }
+                new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+                    @Override
+                    public void onClick(View view, int position) {
+                        Event evento = (Event) adapter.getItem(position);
+                        Intent nextActivity = new Intent(getApplicationContext(), DescripcionActivity.class);
+                        nextActivity.putExtra(EVENTO, evento);
+                        //USERNAME = extras.getString("username");
+                        nextActivity.putExtra("imageUrl", URL_PIC);
+                        nextActivity.putExtra("socialLogin", LOGIN);
+                        nextActivity.putExtra("username", USERNAME);
+                        startActivity(nextActivity);
+                    }
 
-                @Override
-                public void onLongClick(View view, int position) {
+                    @Override
+                    public void onLongClick(View view, int position) {
 
-                }
-            })
+                    }
+                })
         );
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.list_menu, menu);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView search = (SearchView) menu.findItem(R.id.app_bar_search).getActionView();
+            search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+
+            search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Intent mIntent = new Intent(getApplicationContext(), ListActivity.class);
+                    mIntent.putExtra("socialLogin", LOGIN);
+                    mIntent.putExtra("username", USERNAME);
+                    mIntent.putExtra("imageUrl", URL_PIC);
+                    mIntent.putExtra("buscar", query);
+                    mIntent.putExtra(Login.USER, usuario);
+                    startActivity(mIntent);
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+            });
+        }
         return true;
     }
 
